e12d79c0dc9d3e23028bab627e75cac28aed9978
pooja-3star <31280019+pooja-3star@users.noreply.github.com>
2017-08-24 09:31:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io').listen(http);
-var ftpd = require('ftp-server');
-var rn = require('random-number');
-
-ftpd.fsOptions.root = 'http://3starinfo.in//porject/chat_nodeapp/index.js';
 
 app.get('/',function(req,res){
-	var r_num = {  min:  0, max:  100, integer: true};
-	var r_id = rn(r_num);
-	console.log(r_id);
-	res.send('<a href="http://127.0.0.1:8021/:'+r_id+'">id link</a>');
-	//res.sendFile(__dirname + '/index.html');
-	//console.log('hello');
-});
-
-app.get('/:r_id',function(req,res){
 	res.sendFile(__dirname + '/index.html');
-	//console.log('hello');
 });
 
 io.on('connection', function(socket){
@@ -28,7 +14,6 @@ io.on('connection', function(socket){
   });
 });
 
-ftpd.listen(21);
 http.listen(8021,'0.0.0.0', function(){
       console.log('listening on *:8021');
 });
